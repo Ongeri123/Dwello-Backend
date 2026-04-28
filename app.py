21867114eaf621a009dab5ac31b743f61503a77b
Newton Ongeri <213055218+Ongeri123@users.noreply.github.com>
2026-04-28 06:46:23 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app, origins="http://localhost:5173")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
