e8c99ee17c7eb51a4dc2ea367a14e6262c1e9e3a
Newton Ongeri <213055218+Ongeri123@users.noreply.github.com>
2026-05-07 07:43:12 +0300

    feat(Flask app setup) : blueprint registration, CORS config, port 5001

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 
+from routes.upload import upload_bp
+from routes.auth import auth_bp
+from routes.requests import requests_bp
 
 app = Flask(__name__)
-CORS(app, origins="http://localhost:5173")
+CORS(app, origins=["http://localhost:5173"])
+
+app.register_blueprint(upload_bp, url_prefix="/api")
+app.register_blueprint(auth_bp, url_prefix="/api")
+app.register_blueprint(requests_bp, url_prefix="/api")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
